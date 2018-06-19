956dd649626399bbd6b1ac8002a333ecc5ce2c17
zeinabnimer <www.zeinab6@gmail.com>
2018-06-19 20:28:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
-
 def fizzbuzz(number)
-
 if number%3 == 0
   puts "fizz"
 elseif number%5 == 0
 puts "buzz"
 elseif number%15 == 0
   puts "fizzbuzz"
-else "I AM THE KING"
+else ""
 end
 end
 fizzbuzz(14)
