8801f1cbdaeb3e4668875ce8425afc6f11995a58
zeinabnimer <www.zeinab6@gmail.com>
2018-06-19 20:37:56 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+if number%15 == 0
+  puts "FizzBuzz"
+  "FizzBuzz"
+elsif number%5 == 0
+puts "Buzz"
+"Buzz"
+elsif number%3 == 0
+  puts "Fizz"
+  "Fizz"
+else
+  puts "HI"
+end
+end
+fizzbuzz(14)
